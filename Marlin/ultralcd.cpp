07ec95d17c24c2cfc7d5036490d1b032a776be8c
daid <daid303@gmail.com>
2014-08-21 16:03:21 +0200

    Do not show odd double flow in tune. Increase the time waiting till disabling steppers so the bed-calibration in Cura has less issues.

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -385,13 +385,16 @@ static void lcd_tune_menu()
     MENU_ITEM_EDIT(int3, MSG_BED, &target_temperature_bed, 0, BED_MAXTEMP - 15);
 #endif
     MENU_ITEM_EDIT(int3, MSG_FAN_SPEED, &fanSpeed, 0, 255);
+#if EXTRUDERS == 1
     MENU_ITEM_EDIT(int3, MSG_FLOW, &extrudemultiply, 10, 999);
+#else
     MENU_ITEM_EDIT(int3, MSG_FLOW0, &extruder_multiply[0], 10, 999);
 #if TEMP_SENSOR_1 != 0
     MENU_ITEM_EDIT(int3, MSG_FLOW1, &extruder_multiply[1], 10, 999);
 #endif
 #if TEMP_SENSOR_2 != 0
     MENU_ITEM_EDIT(int3, MSG_FLOW2, &extruder_multiply[2], 10, 999);
+#endif
 #endif
 
 #ifdef BABYSTEPPING
