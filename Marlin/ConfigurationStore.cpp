8fd7d096efbdb9411da3b6717c7b804c7f40470b
Christopher Katins <Peyje@users.noreply.github.com>
2018-04-21 10:23:38 +0200

    Now able to store LED values & longer display timeout

--- Marlin/ConfigurationStore.cpp
+++ Marlin/ConfigurationStore.cpp
@@ -6,22 +6,22 @@
 
 void _EEPROM_writeData(int &pos, uint8_t* value, uint8_t size)
 {
-    do
-    {
-        eeprom_write_byte((unsigned char*)pos, *value);
-        pos++;
-        value++;
-    }while(--size);
+  do
+  {
+    eeprom_write_byte((unsigned char*)pos, *value);
+    pos++;
+    value++;
+  }while(--size);
 }
 #define EEPROM_WRITE_VAR(pos, value) _EEPROM_writeData(pos, (uint8_t*)&value, sizeof(value))
 void _EEPROM_readData(int &pos, uint8_t* value, uint8_t size)
 {
-    do
-    {
-        *value = eeprom_read_byte((unsigned char*)pos);
-        pos++;
-        value++;
-    }while(--size);
+  do
+  {
+    *value = eeprom_read_byte((unsigned char*)pos);
+    pos++;
+    value++;
+  }while(--size);
 }
 #define EEPROM_READ_VAR(pos, value) _EEPROM_readData(pos, (uint8_t*)&value, sizeof(value))
 //======================================================================================
@@ -42,6 +42,14 @@ void _EEPROM_readData(int &pos, uint8_t* value, uint8_t size)
 #else
 #define EEPROM_VERSION "V10"
 #endif
+
+// EEPROM Extension
+// We don't want to invalidate everything when adding variables, so we introduce
+// a sub-version to default only new fields when we add at the end.
+// Although not bullet proof we use a magic cookie to detect first change.
+#define EEPROM_SUBVERSION 1
+#define EEPROM_MAGIC 0x3d
+
 
 #ifdef EEPROM_SETTINGS
 void Config_StoreSettings() 
@@ -79,20 +87,31 @@ void Config_StoreSettings()
   EEPROM_WRITE_VAR(i,absPreheatFanSpeed);
   EEPROM_WRITE_VAR(i,zprobe_zoffset);
   #ifdef PIDTEMP
-    EEPROM_WRITE_VAR(i,Kp);
-    EEPROM_WRITE_VAR(i,Ki);
-    EEPROM_WRITE_VAR(i,Kd);
+  EEPROM_WRITE_VAR(i,Kp);
+  EEPROM_WRITE_VAR(i,Ki);
+  EEPROM_WRITE_VAR(i,Kd);
   #else
-		float dummy = 3000.0f;
-    EEPROM_WRITE_VAR(i,dummy);
-		dummy = 0.0f;
-    EEPROM_WRITE_VAR(i,dummy);
-    EEPROM_WRITE_VAR(i,dummy);
+  float dummy = 3000.0f;
+  EEPROM_WRITE_VAR(i,dummy);
+  dummy = 0.0f;
+  EEPROM_WRITE_VAR(i,dummy);
+  EEPROM_WRITE_VAR(i,dummy);
   #endif
   #ifndef DOGLCD
-    int lcd_contrast = 32;
+  int lcd_contrast = 32;
   #endif
-  EEPROM_WRITE_VAR(i,lcd_contrast);
+    EEPROM_WRITE_VAR(i,lcd_contrast);
+    // Start EEPROM Extension
+  uint8_t b = EEPROM_MAGIC;
+  EEPROM_WRITE_VAR(i,b);
+  b = EEPROM_SUBVERSION;
+  EEPROM_WRITE_VAR(i,b);
+
+   #if !(defined(LED_PIN) && LED_PIN > -1)
+  int ledPwm;
+  #endif
+  EEPROM_WRITE_VAR(i,ledPwm);
+
   char ver2[4]=EEPROM_VERSION;
   i=EEPROM_OFFSET;
   EEPROM_WRITE_VAR(i,ver2); // validate data
@@ -105,81 +124,88 @@ void Config_StoreSettings()
 #ifndef DISABLE_M503
 void Config_PrintSettings()
 {  // Always have this function, even with EEPROM_SETTINGS disabled, the current values will be shown
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Steps per unit:");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M92 X",axis_steps_per_unit[0]);
-    SERIAL_ECHOPAIR(" Y",axis_steps_per_unit[1]);
-    SERIAL_ECHOPAIR(" Z",axis_steps_per_unit[2]);
-    SERIAL_ECHOPAIR(" E",axis_steps_per_unit[3]);
-    SERIAL_ECHOLN("");
-      
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Maximum feedrates (mm/s):");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M203 X",max_feedrate[0]);
-    SERIAL_ECHOPAIR(" Y",max_feedrate[1] ); 
-    SERIAL_ECHOPAIR(" Z", max_feedrate[2] ); 
-    SERIAL_ECHOPAIR(" E", max_feedrate[3]);
-    SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Steps per unit:");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M92 X",axis_steps_per_unit[0]);
+  SERIAL_ECHOPAIR(" Y",axis_steps_per_unit[1]);
+  SERIAL_ECHOPAIR(" Z",axis_steps_per_unit[2]);
+  SERIAL_ECHOPAIR(" E",axis_steps_per_unit[3]);
+  SERIAL_ECHOLN("");
 
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Maximum Acceleration (mm/s2):");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M201 X" ,max_acceleration_units_per_sq_second[0] ); 
-    SERIAL_ECHOPAIR(" Y" , max_acceleration_units_per_sq_second[1] ); 
-    SERIAL_ECHOPAIR(" Z" ,max_acceleration_units_per_sq_second[2] );
-    SERIAL_ECHOPAIR(" E" ,max_acceleration_units_per_sq_second[3]);
-    SERIAL_ECHOLN("");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Acceleration: S=acceleration, T=retract acceleration");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M204 S",acceleration ); 
-    SERIAL_ECHOPAIR(" T" ,retract_acceleration);
-    SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Maximum feedrates (mm/s):");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M203 X",max_feedrate[0]);
+  SERIAL_ECHOPAIR(" Y",max_feedrate[1] ); 
+  SERIAL_ECHOPAIR(" Z", max_feedrate[2] ); 
+  SERIAL_ECHOPAIR(" E", max_feedrate[3]);
+  SERIAL_ECHOLN("");
 
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Advanced variables: S=Min feedrate (mm/s), T=Min travel feedrate (mm/s), B=minimum segment time (ms), X=maximum XY jerk (mm/s),  Z=maximum Z jerk (mm/s),  E=maximum E jerk (mm/s)");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M205 S",minimumfeedrate ); 
-    SERIAL_ECHOPAIR(" T" ,mintravelfeedrate ); 
-    SERIAL_ECHOPAIR(" B" ,minsegmenttime ); 
-    SERIAL_ECHOPAIR(" X" ,max_xy_jerk ); 
-    SERIAL_ECHOPAIR(" Z" ,max_z_jerk);
-    SERIAL_ECHOPAIR(" E" ,max_e_jerk);
-    SERIAL_ECHOLN(""); 
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Maximum Acceleration (mm/s2):");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M201 X" ,max_acceleration_units_per_sq_second[0] ); 
+  SERIAL_ECHOPAIR(" Y" , max_acceleration_units_per_sq_second[1] ); 
+  SERIAL_ECHOPAIR(" Z" ,max_acceleration_units_per_sq_second[2] );
+  SERIAL_ECHOPAIR(" E" ,max_acceleration_units_per_sq_second[3]);
+  SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Acceleration: S=acceleration, T=retract acceleration");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M204 S",acceleration ); 
+  SERIAL_ECHOPAIR(" T" ,retract_acceleration);
+  SERIAL_ECHOLN("");
 
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Home offset (mm):");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M206 X",add_homeing[0] );
-    SERIAL_ECHOPAIR(" Y" ,add_homeing[1] );
-    SERIAL_ECHOPAIR(" Z" ,add_homeing[2] );
-    SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Advanced variables: S=Min feedrate (mm/s), T=Min travel feedrate (mm/s), B=minimum segment time (ms), X=maximum XY jerk (mm/s),  Z=maximum Z jerk (mm/s),  E=maximum E jerk (mm/s)");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M205 S",minimumfeedrate ); 
+  SERIAL_ECHOPAIR(" T" ,mintravelfeedrate ); 
+  SERIAL_ECHOPAIR(" B" ,minsegmenttime ); 
+  SERIAL_ECHOPAIR(" X" ,max_xy_jerk ); 
+  SERIAL_ECHOPAIR(" Z" ,max_z_jerk);
+  SERIAL_ECHOPAIR(" E" ,max_e_jerk);
+  SERIAL_ECHOLN(""); 
+
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Home offset (mm):");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M206 X",add_homeing[0] );
+  SERIAL_ECHOPAIR(" Y" ,add_homeing[1] );
+  SERIAL_ECHOPAIR(" Z" ,add_homeing[2] );
+  SERIAL_ECHOLN("");
 #ifdef DELTA
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("Endstop adjustement (mm):");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("  M666 X",endstop_adj[0] );
-    SERIAL_ECHOPAIR(" Y" ,endstop_adj[1] );
-    SERIAL_ECHOPAIR(" Z" ,endstop_adj[2] );
-	SERIAL_ECHOLN("");
-	SERIAL_ECHO_START;
-	SERIAL_ECHOLNPGM("Delta settings: L=delta_diagonal_rod, R=delta_radius, S=delta_segments_per_second");
-	SERIAL_ECHO_START;
-	SERIAL_ECHOPAIR("  M665 L",delta_diagonal_rod );
-	SERIAL_ECHOPAIR(" R" ,delta_radius );
-	SERIAL_ECHOPAIR(" S" ,delta_segments_per_second );
-	SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Endstop adjustement (mm):");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M666 X",endstop_adj[0] );
+  SERIAL_ECHOPAIR(" Y" ,endstop_adj[1] );
+  SERIAL_ECHOPAIR(" Z" ,endstop_adj[2] );
+  SERIAL_ECHOLN("");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Delta settings: L=delta_diagonal_rod, R=delta_radius, S=delta_segments_per_second");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M665 L",delta_diagonal_rod );
+  SERIAL_ECHOPAIR(" R" ,delta_radius );
+  SERIAL_ECHOPAIR(" S" ,delta_segments_per_second );
+  SERIAL_ECHOLN("");
 #endif
 #ifdef PIDTEMP
-    SERIAL_ECHO_START;
-    SERIAL_ECHOLNPGM("PID settings:");
-    SERIAL_ECHO_START;
-    SERIAL_ECHOPAIR("   M301 P",Kp); 
-    SERIAL_ECHOPAIR(" I" ,unscalePID_i(Ki)); 
-    SERIAL_ECHOPAIR(" D" ,unscalePID_d(Kd));
-    SERIAL_ECHOLN(""); 
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("PID settings:");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("   M301 P",Kp); 
+  SERIAL_ECHOPAIR(" I" ,unscalePID_i(Ki)); 
+  SERIAL_ECHOPAIR(" D" ,unscalePID_d(Kd));
+  SERIAL_ECHOLN(""); 
+#endif
+    #if defined(LED_PIN) && LED_PIN > -1
+  SERIAL_ECHO_START;
+  SERIAL_ECHOLNPGM("Led brightness:");
+  SERIAL_ECHO_START;
+  SERIAL_ECHOPAIR("  M42 ", (unsigned long)ledPwm);
+  SERIAL_ECHOLN("");
 #endif
 } 
 #endif
@@ -188,84 +214,112 @@ void Config_PrintSettings()
 #ifdef EEPROM_SETTINGS
 void Config_RetrieveSettings()
 {
-    int i=EEPROM_OFFSET;
-    char stored_ver[4];
-    char ver[4]=EEPROM_VERSION;
+  int i=EEPROM_OFFSET;
+  char stored_ver[4];
+  char ver[4]=EEPROM_VERSION;
     EEPROM_READ_VAR(i,stored_ver); //read stored version
     //  SERIAL_ECHOLN("Version: [" << ver << "] Stored version: [" << stored_ver << "]");
     if (strncmp(ver,stored_ver,3) == 0)
     {
         // version number match
-        EEPROM_READ_VAR(i,axis_steps_per_unit);  
-        EEPROM_READ_VAR(i,max_feedrate);  
-        EEPROM_READ_VAR(i,max_acceleration_units_per_sq_second);
-        
+      EEPROM_READ_VAR(i,axis_steps_per_unit);  
+      EEPROM_READ_VAR(i,max_feedrate);  
+      EEPROM_READ_VAR(i,max_acceleration_units_per_sq_second);
+
         // steps per sq second need to be updated to agree with the units per sq second (as they are what is used in the planner)
-		reset_acceleration_rates();
-        
-        EEPROM_READ_VAR(i,acceleration);
-        EEPROM_READ_VAR(i,retract_acceleration);
-        EEPROM_READ_VAR(i,minimumfeedrate);
-        EEPROM_READ_VAR(i,mintravelfeedrate);
-        EEPROM_READ_VAR(i,minsegmenttime);
-        EEPROM_READ_VAR(i,max_xy_jerk);
-        EEPROM_READ_VAR(i,max_z_jerk);
-        EEPROM_READ_VAR(i,max_e_jerk);
-        EEPROM_READ_VAR(i,add_homeing);
+      reset_acceleration_rates();
+
+      EEPROM_READ_VAR(i,acceleration);
+      EEPROM_READ_VAR(i,retract_acceleration);
+      EEPROM_READ_VAR(i,minimumfeedrate);
+      EEPROM_READ_VAR(i,mintravelfeedrate);
+      EEPROM_READ_VAR(i,minsegmenttime);
+      EEPROM_READ_VAR(i,max_xy_jerk);
+      EEPROM_READ_VAR(i,max_z_jerk);
+      EEPROM_READ_VAR(i,max_e_jerk);
+      EEPROM_READ_VAR(i,add_homeing);
         #ifdef DELTA
-		EEPROM_READ_VAR(i,endstop_adj);
-		EEPROM_READ_VAR(i,delta_radius);
-		EEPROM_READ_VAR(i,delta_diagonal_rod);
-		EEPROM_READ_VAR(i,delta_segments_per_second);
+      EEPROM_READ_VAR(i,endstop_adj);
+      EEPROM_READ_VAR(i,delta_radius);
+      EEPROM_READ_VAR(i,delta_diagonal_rod);
+      EEPROM_READ_VAR(i,delta_segments_per_second);
         #endif
         #ifndef ULTIPANEL
-        int plaPreheatHotendTemp, plaPreheatHPBTemp, plaPreheatFanSpeed;
-        int absPreheatHotendTemp, absPreheatHPBTemp, absPreheatFanSpeed;
+      int plaPreheatHotendTemp, plaPreheatHPBTemp, plaPreheatFanSpeed;
+      int absPreheatHotendTemp, absPreheatHPBTemp, absPreheatFanSpeed;
         #endif
-        EEPROM_READ_VAR(i,plaPreheatHotendTemp);
-        EEPROM_READ_VAR(i,plaPreheatHPBTemp);
-        EEPROM_READ_VAR(i,plaPreheatFanSpeed);
-        EEPROM_READ_VAR(i,absPreheatHotendTemp);
-        EEPROM_READ_VAR(i,absPreheatHPBTemp);
-        EEPROM_READ_VAR(i,absPreheatFanSpeed);
-        EEPROM_READ_VAR(i,zprobe_zoffset);
+      EEPROM_READ_VAR(i,plaPreheatHotendTemp);
+      EEPROM_READ_VAR(i,plaPreheatHPBTemp);
+      EEPROM_READ_VAR(i,plaPreheatFanSpeed);
+      EEPROM_READ_VAR(i,absPreheatHotendTemp);
+      EEPROM_READ_VAR(i,absPreheatHPBTemp);
+      EEPROM_READ_VAR(i,absPreheatFanSpeed);
+      EEPROM_READ_VAR(i,zprobe_zoffset);
         #ifndef PIDTEMP
-        float Kp,Ki,Kd;
+      float Kp,Ki,Kd;
         #endif
         // do not need to scale PID values as the values in EEPROM are already scaled		
-        EEPROM_READ_VAR(i,Kp);
-        EEPROM_READ_VAR(i,Ki);
-        EEPROM_READ_VAR(i,Kd);
+      EEPROM_READ_VAR(i,Kp);
+      EEPROM_READ_VAR(i,Ki);
+      EEPROM_READ_VAR(i,Kd);
         #ifndef DOGLCD
-        int lcd_contrast;
+      int lcd_contrast;
         #endif
-        EEPROM_READ_VAR(i,lcd_contrast);
+      EEPROM_READ_VAR(i,lcd_contrast);
+
+        // Get sub-version
+      uint8_t sub_version;
+      EEPROM_READ_VAR(i,sub_version);
+      if (sub_version == EEPROM_MAGIC) {
+          // Magic matches, get sub-version
+        EEPROM_READ_VAR(i,sub_version);
+      } else {
+          // Assume first call
+        sub_version = 0;
+      }
+      if (sub_version < 1) {
+          #if defined(LED_PIN) && LED_PIN > -1
+        ledPwm=0;
+          #endif
+      } else {
+          #if !(defined(LED_PIN) && LED_PIN > -1)
+        int ledPwm;
+          #endif
+        EEPROM_READ_VAR(i,ledPwm);
+      }
+        #if defined(LED_PIN) && LED_PIN > -1
+      if (ledInit) {
+            // Only setup led if we passed the init
+        pinMode(LED_PIN, OUTPUT);
+        analogWrite(LED_PIN, ledPwm);
+      }
+        #endif
 
 		// Call updatePID (similar to when we have processed M301)
-		updatePID();
-        SERIAL_ECHO_START;
-        SERIAL_ECHOLNPGM("Stored settings retrieved");
+      updatePID();
+      SERIAL_ECHO_START;
+      SERIAL_ECHOLNPGM("Stored settings retrieved");
     }
     else
     {
-        Config_ResetDefault();
+      Config_ResetDefault();
     }
     #ifdef EEPROM_CHITCHAT
-      Config_PrintSettings();
+    Config_PrintSettings();
     #endif
-}
+  }
 #endif
 
-void Config_ResetDefault()
-{
+  void Config_ResetDefault()
+  {
     float tmp1[]=DEFAULT_AXIS_STEPS_PER_UNIT;
     float tmp2[]=DEFAULT_MAX_FEEDRATE;
     long tmp3[]=DEFAULT_MAX_ACCELERATION;
     for (short i=0;i<4;i++) 
     {
-        axis_steps_per_unit[i]=tmp1[i];  
-        max_feedrate[i]=tmp2[i];  
-        max_acceleration_units_per_sq_second[i]=tmp3[i];
+      axis_steps_per_unit[i]=tmp1[i];  
+      max_feedrate[i]=tmp2[i];  
+      max_acceleration_units_per_sq_second[i]=tmp3[i];
     }
     
     // steps per sq second need to be updated to agree with the units per sq second
@@ -281,11 +335,11 @@ void Config_ResetDefault()
     max_e_jerk=DEFAULT_EJERK;
     add_homeing[0] = add_homeing[1] = add_homeing[2] = 0;
 #ifdef DELTA
-	endstop_adj[0] = endstop_adj[1] = endstop_adj[2] = 0;
-	delta_radius= DELTA_RADIUS;
-	delta_diagonal_rod= DELTA_DIAGONAL_ROD;
-	delta_segments_per_second= DELTA_SEGMENTS_PER_SECOND;
-	recalc_delta_settings(delta_radius, delta_diagonal_rod);
+    endstop_adj[0] = endstop_adj[1] = endstop_adj[2] = 0;
+    delta_radius= DELTA_RADIUS;
+    delta_diagonal_rod= DELTA_DIAGONAL_ROD;
+    delta_segments_per_second= DELTA_SEGMENTS_PER_SECOND;
+    recalc_delta_settings(delta_radius, delta_diagonal_rod);
 #endif
 #ifdef ULTIPANEL
     plaPreheatHotendTemp = PLA_PREHEAT_HOTEND_TEMP;
@@ -314,7 +368,12 @@ void Config_ResetDefault()
 #endif//PID_ADD_EXTRUSION_RATE
 #endif//PIDTEMP
 
-SERIAL_ECHO_START;
-SERIAL_ECHOLNPGM("Hardcoded Default Settings Loaded");
+    #if defined(LED_PIN) && LED_PIN > -1
+    ledPwm = 0;
+    analogWrite(LED_PIN, 0);
+#endif
 
-}
+    SERIAL_ECHO_START;
+    SERIAL_ECHOLNPGM("Hardcoded Default Settings Loaded");
+
+  }
